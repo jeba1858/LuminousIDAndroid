a51e1e6509b8d99a0e03075b1f519fbe3ea86741
jeba1858 <jeba1858@colorado.edu>
2017-01-31 17:14:46 -0700

    Add login activity and home screen

---
+++ app/src/main/java/com/luminousid/luminousid/Home_screen.java
@@ -0,0 +1,13 @@
+package com.luminousid.luminousid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
